593618657fb9f07b8fbb2d9a994301d96973fef6
Wonwoo Choi <onechoo1106@gmail.com>
2022-07-26 01:40:42 +1000

    last try

--- histogram.py
+++ histogram.py
@@ -1,1 +1,26 @@
-print("histogram thing?")
+
+# what is phillip.txt?
+def histogram():
+    
+    result={}
+    #input_name=input("Enter file name:")
+    txt=open('test.txt','r')
+    texts=txt.readlines()
+    for i in range(len(texts)):
+        #print(texts[i].split())
+        text=texts[i].split()
+        for i in range(len(text)):
+            if text[i] in result:
+                result[text[i]]+=1
+            elif text[i] not in result:
+                result[text[i]]=1
+    #print(result)
+    for key,value in result.items():
+        print(key,value)
+        
+    txt.close()
+    
+    
+    #print("Output: ",result)
+
+histogram()
